80a68230da4247fa9c634f44ba5539bf75874815
tarasov artem <temich080@gmail.com>
2021-06-14 17:16:17 +0300

    Module Next Gen JS Code ended

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,3 @@
 "use strict";
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["AUTHOR"] = 1] = "AUTHOR";
-    Role[Role["READ_WRITE"] = 2] = "READ_WRITE";
-})(Role || (Role = {}));
-var person = {
-    role: Role.ADMIN
-};
-console.log(person.role);
-console.log(person.role);
-console.log(person.role);
+console.log('It works12');
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-enum Role {ADMIN, AUTHOR, READ_WRITE}
+// 47 Rest Parameters
+// const add = (...others: number[]) => {
+//     let result = 0;
+//     for (let num of others) result += num;
+//     return result;
+// }
+// OR
+// const add = (...numbers: number[]) => {
+//     return numbers.reduce((curResult, curValue) => {
+//         return curResult + curValue;
+//     }, 0);
+// }
+// console.log(add(22, 11, 50, 50, 100))
 
-let person = {
-    role: Role.ADMIN
-};
 
-console.log(person.role);
-console.log(person.role);
-console.log(person.role);
-console.log(person.role);
-console.log(person.role);
+// 48 Arrays and Objects Destructuring
+// const hobbies = ['football', 'hockey'];
+// const [ hobby1, hobby2 ] = hobbies;
+// const person = {
+//     firstName: 'Alex',
+//     age: 26
+// }
+// const {firstName: userName, age} = person;
+// NB: in this case userName is alias for firstName;
+// console.log(userName, age);
+
+// End of module
+
+
+// Module Classes and Interfaces (50-73) - 1h30m
+// 51 What are classes
+
+console.log('It works12')
